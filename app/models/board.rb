562c18763fbd0c89c3136b7036131fc1c9bd8436
natdjerf <nadjerf@gmail.com>
2016-05-10 16:37:54 -0400

    board model revision

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 # Boards are owned by users
 class Board < ActiveRecord::Base
   belongs_to :user, inverse_of: :boards
-  has_many :cheese_additions
+  has_many :cheese_additions, dependent: :destroy
   has_many :cheeses, through: :cheese_additions
 end
